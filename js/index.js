7238952ab69999d793c98dec55f7cbb0537ea088
Tayro <adventuregamr@gmail.com>
2022-12-05 18:52:05 +0100

    Added chapter 9 + Responsiveness

--- js/index.js
+++ js/index.js
@@ -4,6 +4,19 @@ const mainNavLinks = document.querySelectorAll(".navLink");
 const lightBulbText = document.querySelector(".lightBulb p");
 const tutImgs = document.querySelectorAll("img.tutImg");
 
+for(i = 0; tutImgs.length > i; i++){
+    //console.log(tutImgs[i].width);
+    if(tutImgs[i].width == 300){
+        tutImgs[i].classList.add('width300');
+    }else if(tutImgs[i].width == 400){
+        tutImgs[i].classList.add('width400');
+    }else if(tutImgs[i].width == 500){
+        tutImgs[i].classList.add('width500');
+    }else if(tutImgs[i].width == 600){
+        tutImgs[i].classList.add('width600');
+    }
+}
+
 function changeTheme(){
     mainPage.forEach((element) => {
         element.classList.toggle("darkTheme");
